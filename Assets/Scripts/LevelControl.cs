eaf3f4263976ec7b318bf68adde6ffdbb8a02c80
Yusuf EROGLU <eroglu.yusuf@yahoo.com>
2020-06-16 06:45:32 +0300

    add level controller

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLevelAchived())
+        {
+            Debug.Log("bölüm bitti");
+        }
+        else if (isLevelFailed())
+        {
+            Debug.Log("Bölüm Başarısız");
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Male"))
+        {
+            ScoreManager.maleOntheButton = true;
+        }
+        else if (other.tag.Equals("Female"))
+        {
+            ScoreManager.femaleOntheButton = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Male"))
+        {
+            ScoreManager.maleOntheButton = false;
+        }
+        else if (other.tag.Equals("Female"))
+        {
+            ScoreManager.femaleOntheButton = false;
+        }
+    }
+    private bool isLevelAchived()
+    {
+        bool located = ScoreManager.femaleOntheButton && ScoreManager.maleOntheButton;
+        bool loaded = ScoreManager.femaleLoad == 2 && ScoreManager.maleLoad == 2;
+        if (located && loaded)
+        {
+            return true;
+        }
+        return false;
+    }
+    private bool isLevelFailed()
+    {
+        bool fall = ScoreManager.femaleFalling || ScoreManager.maleFalling;
+        return fall;
+    }
+
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreManager
+public class ScoreManager: ScriptableObject
 {
+
     public static int maleLoad = 0;
     public static int femaleLoad = 0;
+
+    public static bool maleOntheButton = false;
+    public static bool femaleOntheButton = false;
+
+    public static bool maleFalling = false;
+    public static bool femaleFalling = false;
+
+    
 }
